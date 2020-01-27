fac584e70c2abef9eb5df7ca53e0ef070d07cd92
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-01-27 18:00:24 +0900

    Add favicon koo

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import favicon from "../images/favicon.ico"
+import Helmet from "react-helmet"
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
@@ -52,22 +53,27 @@ class Layout extends React.Component {
       )
     }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <>
+        <Helmet>
+          <link rel="icon" href={favicon} />
+        </Helmet>
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <header>{header}</header>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </>
     )
   }
 }
