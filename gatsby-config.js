bfc5fd02e2adda9abc4411f7d2992476e8db9e58
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-02-06 02:05:55 +0900

    Update Text Color

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,17 @@ module.exports = {
         shortname: "kooboyblog",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          "Montserrrat",
+          `limelight`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
     `gatsby-plugin-lodash`,
     `gatsby-plugin-styled-components`,
     "gatsby-plugin-dark-mode",
